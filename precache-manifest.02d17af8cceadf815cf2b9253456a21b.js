cc19683e976d416cbcbdbb5539e5e8f30752b63b
Astromium <interstellarspace2000@gmail.com>
2019-10-13 20:31:02 +0100

    Updates

---
+++ precache-manifest.02d17af8cceadf815cf2b9253456a21b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4811cbc2f50706086641e594b7648e",
+    "url": "/djelfaDoctors/index.html"
+  },
+  {
+    "revision": "331311623d25a86aefdd",
+    "url": "/djelfaDoctors/static/css/main.f408c272.chunk.css"
+  },
+  {
+    "revision": "15eff58edb4f903d233a",
+    "url": "/djelfaDoctors/static/js/2.a3cce119.chunk.js"
+  },
+  {
+    "revision": "331311623d25a86aefdd",
+    "url": "/djelfaDoctors/static/js/main.66b998c0.chunk.js"
+  },
+  {
+    "revision": "083396069ee54a6b4513",
+    "url": "/djelfaDoctors/static/js/runtime-main.37ad212b.js"
+  },
+  {
+    "revision": "499b1b313242cb5600592742ff763ce8",
+    "url": "/djelfaDoctors/static/media/bones.499b1b31.png"
+  },
+  {
+    "revision": "3ac0b7659fd87b98d2009af52888a356",
+    "url": "/djelfaDoctors/static/media/cardio.3ac0b765.jpg"
+  },
+  {
+    "revision": "db7ffa64894abc9882a5a5282e43eb55",
+    "url": "/djelfaDoctors/static/media/chest.db7ffa64.png"
+  },
+  {
+    "revision": "f2fb2b51a04138e41b0813386624235d",
+    "url": "/djelfaDoctors/static/media/cologue.f2fb2b51.jpg"
+  },
+  {
+    "revision": "7dfc2211ea3c68d280976db3b2658ce1",
+    "url": "/djelfaDoctors/static/media/dentist.7dfc2211.jpg"
+  },
+  {
+    "revision": "c4555ddc6cf1e045ad6518e0f75eb4fa",
+    "url": "/djelfaDoctors/static/media/derma.c4555ddc.jpg"
+  },
+  {
+    "revision": "df6005c4a3c66e1f87fea8944033cc7f",
+    "url": "/djelfaDoctors/static/media/eye.df6005c4.png"
+  },
+  {
+    "revision": "ca3a6ed8221b02bd9a216f6254fdde28",
+    "url": "/djelfaDoctors/static/media/internals.ca3a6ed8.jpg"
+  },
+  {
+    "revision": "6c581ec1ec0654ea979d35ab2a66dfc9",
+    "url": "/djelfaDoctors/static/media/neuro.6c581ec1.jpg"
+  },
+  {
+    "revision": "ed20bcee238c7a9437f183b4c0f42bc8",
+    "url": "/djelfaDoctors/static/media/orl.ed20bcee.png"
+  },
+  {
+    "revision": "c915d2434753f7477bee7a6742d2baa0",
+    "url": "/djelfaDoctors/static/media/pediatre.c915d243.png"
+  },
+  {
+    "revision": "d186816fb1f213246ecb81985e5185b4",
+    "url": "/djelfaDoctors/static/media/sugar.d186816f.png"
+  },
+  {
+    "revision": "bc4dd336fca56f3e76941c880fd4a8d6",
+    "url": "/djelfaDoctors/static/media/surgeon.bc4dd336.png"
+  },
+  {
+    "revision": "3ed79f1b25d407a14d560dcc14772034",
+    "url": "/djelfaDoctors/static/media/undraw_medicine_b1ol.3ed79f1b.svg"
+  },
+  {
+    "revision": "c2ef908953f33e65cfe28bc6bbc98550",
+    "url": "/djelfaDoctors/static/media/urologie.c2ef9089.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Djelfa-Doctors/precache-manifest.cccdf5a2c44ffddf47396977aa2df14c.js"
+  "/djelfaDoctors/precache-manifest.02d17af8cceadf815cf2b9253456a21b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Djelfa-Doctors/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/djelfaDoctors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
