ae947c622b7a81fad1ae39f54b573037db0bba7a
Astromium <interstellarspace2000@gmail.com>
2019-10-13 19:25:14 +0100

    Updates

---
+++ precache-manifest.2eac3a6995f3557941385203c30ad7b8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492a8378b5471b0fe2dabbb984ca575e",
+    "url": "/Djelfa-Doctors/index.html"
+  },
+  {
+    "revision": "ffec9421f4ecc6dfa597",
+    "url": "/Djelfa-Doctors/static/css/main.f408c272.chunk.css"
+  },
+  {
+    "revision": "d98c33ceee54e3d47acf",
+    "url": "/Djelfa-Doctors/static/js/2.50853506.chunk.js"
+  },
+  {
+    "revision": "ffec9421f4ecc6dfa597",
+    "url": "/Djelfa-Doctors/static/js/main.6bee6f50.chunk.js"
+  },
+  {
+    "revision": "4b1aac343bede2204d5b",
+    "url": "/Djelfa-Doctors/static/js/runtime-main.b0cff466.js"
+  },
+  {
+    "revision": "499b1b313242cb5600592742ff763ce8",
+    "url": "/Djelfa-Doctors/static/media/bones.499b1b31.png"
+  },
+  {
+    "revision": "3ac0b7659fd87b98d2009af52888a356",
+    "url": "/Djelfa-Doctors/static/media/cardio.3ac0b765.jpg"
+  },
+  {
+    "revision": "db7ffa64894abc9882a5a5282e43eb55",
+    "url": "/Djelfa-Doctors/static/media/chest.db7ffa64.png"
+  },
+  {
+    "revision": "f2fb2b51a04138e41b0813386624235d",
+    "url": "/Djelfa-Doctors/static/media/cologue.f2fb2b51.jpg"
+  },
+  {
+    "revision": "7dfc2211ea3c68d280976db3b2658ce1",
+    "url": "/Djelfa-Doctors/static/media/dentist.7dfc2211.jpg"
+  },
+  {
+    "revision": "c4555ddc6cf1e045ad6518e0f75eb4fa",
+    "url": "/Djelfa-Doctors/static/media/derma.c4555ddc.jpg"
+  },
+  {
+    "revision": "df6005c4a3c66e1f87fea8944033cc7f",
+    "url": "/Djelfa-Doctors/static/media/eye.df6005c4.png"
+  },
+  {
+    "revision": "ca3a6ed8221b02bd9a216f6254fdde28",
+    "url": "/Djelfa-Doctors/static/media/internals.ca3a6ed8.jpg"
+  },
+  {
+    "revision": "6c581ec1ec0654ea979d35ab2a66dfc9",
+    "url": "/Djelfa-Doctors/static/media/neuro.6c581ec1.jpg"
+  },
+  {
+    "revision": "ed20bcee238c7a9437f183b4c0f42bc8",
+    "url": "/Djelfa-Doctors/static/media/orl.ed20bcee.png"
+  },
+  {
+    "revision": "c915d2434753f7477bee7a6742d2baa0",
+    "url": "/Djelfa-Doctors/static/media/pediatre.c915d243.png"
+  },
+  {
+    "revision": "d186816fb1f213246ecb81985e5185b4",
+    "url": "/Djelfa-Doctors/static/media/sugar.d186816f.png"
+  },
+  {
+    "revision": "bc4dd336fca56f3e76941c880fd4a8d6",
+    "url": "/Djelfa-Doctors/static/media/surgeon.bc4dd336.png"
+  },
+  {
+    "revision": "3ed79f1b25d407a14d560dcc14772034",
+    "url": "/Djelfa-Doctors/static/media/undraw_medicine_b1ol.3ed79f1b.svg"
+  },
+  {
+    "revision": "c2ef908953f33e65cfe28bc6bbc98550",
+    "url": "/Djelfa-Doctors/static/media/urologie.c2ef9089.jpg"
+  }
+]);
